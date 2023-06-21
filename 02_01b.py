7f63dfedf591fd67ac136f56e82b92c81126dd00
Vinsensius <84225220+VxVinsensius0625@users.noreply.github.com>
2023-06-21 03:38:46 +0000

    editing all files

--- 02_01b.py
+++ 02_01b.py
@@ -1,8 +1,21 @@
-from collections import Counter
+from collections import Counter, deque
+
+"""
+Storing user's most recent food orders 
+Only store maximum 5 food orders
+"""
 
 
 def main():
+    foods = deque(maxlen=5)
     # add code here
+    foods.append("STA001")
+    foods_ordered = ["DES001", "ENT012", "ENT001", "DES011"]
+    foods.extend(foods_ordered)  # multiple object
+    foods.append("DES002")
+    # ^will automatically remove the left most element since the max lengt is 5
+    # when using appendleft, will pop the item from the opposite side if max length is reached
+    print(foods)
     return
 
 
--- 02_03b.py
+++ 02_03b.py
@@ -1,7 +1,39 @@
+from collections import deque
+
+"""
+Create Task list with deque using append and
+"""
+
+
+class Todolist:
+    def __init__(self, tasklist: list):
+        self.tasks = deque(tasklist)
+
+    def adding(self, tasks_list, priority=False):
+        if priority == True:
+            self.tasks.extendleft(tasks_list)
+        else:
+            self.tasks.extend(tasks_list)
+
+    def printing(self):
+        print(f"The tasks are {', '.join(self.tasks)}")
+
+    def completing(self, task):
+        for tasks in task:
+            self.tasks.remove(tasks)
+        print(f"Finished task: {', '.join(task)}")
+
 
 def main():
-    #add code here
+    # add code here
+    tasks_list = Todolist(["HW", "Bunny Stream"])
+    tasks_list.adding(["Themo FE"])
+    tasks_list.adding(["Hackerrank"], priority=True)
+    tasks_list.printing()
+    tasks_list.completing(["Bunny Stream", "Hackerrank"])
+    tasks_list.printing()
     return
+
 
 if __name__ == "__main__":
     main()
